b86e2b03256684a6d22a5bf0b785c4bb9463e545
Jason Schriver <jschriv@uw.edu>
2016-12-01 04:31:14 +0000

    update get_ips

--- get_ips.sh
+++ get_ips.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 PUBLIC=`curl -s https://cac.schriver.us/ip.php`
 #echo "Public IP is "$PUBLIC
-PRIVATE=`ifconfig wlan0 2>/dev/null|awk '/inet addr:/ {print $2}'|sed 's/addr://'`
+PRIVATE=`sudo ifconfig wlan0 2>/dev/null|awk '/inet addr:/ {print $2}'|sed 's/addr://'`
 #echo "Private IP is "$PRIVATE
 
 echo "Public: "$PUBLIC" Private: "$PRIVATE
