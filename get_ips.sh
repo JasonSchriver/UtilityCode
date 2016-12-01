392b2d2388073b15a04bf32f7dc5e4e59fba5f7d
Jason Schriver <jschriv@uw.edu>
2016-12-01 04:24:09 +0000

    HTTPS

--- get_ips.sh
+++ get_ips.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-PUBLIC=`curl -s cac.schriver.us/ip.php`
+PUBLIC=`curl -s https://cac.schriver.us/ip.php`
 #echo "Public IP is "$PUBLIC
 PRIVATE=`ifconfig wlan0 2>/dev/null|awk '/inet addr:/ {print $2}'|sed 's/addr://'`
 #echo "Private IP is "$PRIVATE
