d1ac9420fd3d69090067ebf4ecf164de765202b3
Jason Schriver <jschriv@uw.edu>
2018-01-21 11:30:08 -0800

    modified location of ip script

--- get_ips.sh
+++ get_ips.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-PUBLIC=`curl -s https://cac.schriver.us/ip.php`
+PUBLIC=`curl -s https://blep.tech/ip.php`
 #echo "Public IP is "$PUBLIC
 PRIVATE=`sudo ifconfig wlan0 2>/dev/null|awk '/inet addr:/ {print $2}'|sed 's/addr://'`
 #echo "Private IP is "$PRIVATE
